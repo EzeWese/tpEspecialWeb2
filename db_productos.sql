43946ee5fde3eba00e1d0491cee8949f2713998e
Ezequiel Wesenack <ezequiel.wese@gmail.com>
2018-10-10 15:52:19 -0300

    Agrego db

--- db_productos.sql
+++ db_productos.sql
@@ -2,8 +2,8 @@
 -- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 09-09-2018 a las 16:46:47
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-10-2018 a las 20:51:28
 -- Versión del servidor: 10.1.32-MariaDB
 -- Versión de PHP: 7.2.5
 
@@ -34,6 +34,14 @@ CREATE TABLE `categoria` (
   `descripcion` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id_categoria`, `nombre`, `descripcion`) VALUES
+(1, 'Mates', 'Deferentes tipos de mates. De calabaza, plata o alpaca, de guampa o asta, de silicona, de madera y de vidrio o cerámica.'),
+(2, 'Bombillas', 'Tipos de bombillas: de plata, enchapadas de oro, de alpaca y de acero inoxidable.');
+
 -- --------------------------------------------------------
 
 --
@@ -43,11 +51,38 @@ CREATE TABLE `categoria` (
 CREATE TABLE `producto` (
   `id_producto` int(11) NOT NULL,
   `id_categoria` int(11) NOT NULL,
-  `categoria` varchar(50) NOT NULL,
   `nombre` varchar(50) NOT NULL,
+  `categoria` varchar(50) NOT NULL,
   `precio` double NOT NULL,
   `descripcion` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id_producto`, `id_categoria`, `nombre`, `categoria`, `precio`, `descripcion`) VALUES
+(4, 2, 'Bombilla de plata', 'Bombillas', 120, 'De acero inox, made in argentina.');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `pass` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nombre`, `pass`) VALUES
+(1, 'pepe', '$2y$10$si8oRTDUH5OhitHUNjmMi.TkFmnaQ5fJGITzoFqNyIeLwkiQVA.wG'),
+(3, 'pepo', '$2y$10$sROsY1Dxj4O/EbbYDHX9XOVeWHCDlFHbfAJZhhU0hrLBREmszIZ8S');
 
 --
 -- Índices para tablas volcadas
@@ -67,6 +102,12 @@ ALTER TABLE `producto`
   ADD KEY `fk_categoria` (`id_categoria`);
 
 --
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -74,13 +115,19 @@ ALTER TABLE `producto`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
